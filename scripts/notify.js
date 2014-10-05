47c9ca5e728d1c1d33bd017ed7dac1a007100d2a
Howon Song <hs459@cornell.edu>
2014-10-04 23:21:16 -0400

    init

--- scripts/notify.js
+++ scripts/notify.js
@@ -1,90 +1,30 @@
 (function(){
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-// notfiy.js
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-var notify = window.notify = {
-    on    : {},
-    event : {
-        message : function(){},
-        click   : function(){},
-        button  : function(){},
-        closed  : function(){}
-    }
-};
 
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-// Populate Event Bindings
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-PUBNUB.each( notify.events, function(event) {
-    notify.on[event] = function(cb) {
-        notify.event[event] = cb || function(){};
-    };
-});
-
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-// Initialize PubNub Broadcast Receiver
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-PUBNUB(PUBNUB.setup).subscribe({
-    channel : PUBNUB.setup.channel,
-    message : receiver
-});
-
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-// Receiver of Remote Broadcast Events
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-function receiver(message) {
-    // Fire User Event for Receiving Notification
-    notify.event.message(message);
-
-    // Is a Desktop Notification?
-    if (!('id' in message && 'options' in message)) return;
-
-    // Display Desktop Notification
-    chrome.notifications.create( message.id, message.options, function() {
-        PUBNUB.events.fire( 'notify.created', message );
+    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    // Initialize PubNub Broadcast Receiver
+    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    PUBNUB(PUBNUB.setup).subscribe({
+        channel : PUBNUB.setup.channel,
+        message : receiver
     });
-}
 
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-// User Clicked on a Button inside the Desktop Notification
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-chrome.notifications.onButtonClicked.addListener(function( id, btn ){
-    notify.event.button({ id : id, button : btn });
-});
+    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    // Receiver of Remote Broadcast Events
+    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    function receiver(message) {
+        chrome.notifications.create("my_noti", message, function(notificationId) {
+            setTimeout(function() {
+                chrome.notifications.clear(notificationId, function(wasCleared) {
+                    console.log(wasCleared);
+                });
+            }, 2000);
+        });
+    }
 
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-// Desktop Notification Displayed to the User
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-chrome.notifications.onDisplayed.addListener(function(id){
-    notify.event.display({ id : id });
-});
-
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-// User Clicked on the Desktop Notification
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-chrome.notifications.onClicked.addListener(function(id){
-    notify.event.display({ id : id });
-});
-
-
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-// User Closed the Desktop Notification
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-notify.on.closed(function(data){
-    console.log(data);
-});
-
-var opt = {
-    iconUrl   : 'images/icon.png',
-    type      : 'basic',
-    title     : 'Primary Title',
-    message   : 'Primary message to display',
-    priority  : 1,
-    buttons: [
-        { title: 'Call',       iconUrl: 'images/icon.png' },
-        { title: 'Send Email', iconUrl: 'images/icon.png' }
-    ]
-};
-
-
+    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    // User Clicked on a Button inside the Desktop Notification
+    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    chrome.notifications.onButtonClicked.addListener(function(notificationId, buttonIndex){
+        console.log("whatever you want here!")
+    });
 })();
--- scripts/pubnub-setup.js
+++ scripts/pubnub-setup.js
@@ -1,5 +1,6 @@
 window.PUBNUB.setup = {
-    channel       : "my-app-channel",
-    subscribe_key : "sub-c-3acbe7f4-24d1-11e4-bb18-02ee2ddab7fe",
+    channel       : "test",
+    subscribe_key : "sub-c-c3f6a9fe-45cb-11e4-a251-02ee2ddab7fe",
+    publish_key   : "pub-c-aa0da548-076b-4c46-bf87-87d2d457368a",
     ssl           : true
 };
